e8046277e2736fc5155c4aa52ea9c9a547ce6359
Alex Smith <alex.smith@redhat.com>
2019-09-02 11:41:02 -0400

    Add solution for exercise 5.2.

---
+++ src/ex5.2.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl -w
+
+# This program does the following:
+# 1. Prompt the user to enter two DNA strings.
+# 2. Concatenate DNA1 + DNA2
+# 3. Print the concatenated DNA string.
+# 4. Print DNA2 aligned to its segment in the concatenated string.
+
+use strict;
+
+my ($dna1, $dna2);
+
+print 'Enter DNA1: ';
+chomp($dna1 = <STDIN>);
+
+print 'Enter DNA2: ';
+chomp($dna2 = <STDIN>);
+
+$dna1 .= $dna2;
+
+
+print $dna1, "\n";
+print ' ' x ((length $dna1) - (length $dna2)), $dna2, "\n";
