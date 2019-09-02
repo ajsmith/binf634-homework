05c39939d7b516220096b9811d97ed45b6afd41a
Alex Smith <alex.smith@redhat.com>
2019-09-01 20:01:01 -0400

    Add solution for exercise 5.4.

---
+++ src/ex5.4.pl
@@ -0,0 +1,25 @@
+use strict;
+
+my $dna;
+my $b;
+my @complement = ();
+
+$dna = join('', <STDIN>);
+
+$dna =~ s/\s*//g;
+
+for (my $i = 0; $i < length $dna; $i++) {
+    $b = substr($dna, $i, 1);
+    if ($b =~ /a/i) {
+        unshift(@complement, 't');
+    } elsif ($b =~ /c/i) {
+        unshift(@complement, 'g');
+    } elsif ($b =~ /g/i) {
+        unshift(@complement, 'c');
+    } elsif ($b =~ /t/i) {
+        unshift(@complement, 'a');
+    }
+}
+
+print 'Orig DNA: ', lc($dna), "\n";
+print 'Comp DNA: ', join('', @complement), "\n";
