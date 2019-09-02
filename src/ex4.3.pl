480df70e3e317a8a6ed43fcc6448a7877b66d18d
Alex Smith <alex.smith@redhat.com>
2019-09-02 12:56:43 -0400

    Add solution for Exercise 4.3.

---
+++ src/ex4.3.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl -w
+
+# Exercise 4.3
+#
+# Print DNA to upper or lowercase.
+
+my $usage = "Usage: ex4.3.pl (upper|lower)";
+
+unless (scalar @ARGV >= 1) {
+    print $usage;
+    exit 1;
+}
+
+my ($mode) = @ARGV;
+$mode = lc($mode);
+
+my $dna = join('', <STDIN>);
+$dna =~ s/\s*//g;
+
+if ($mode eq "lower") {
+    $dna =~ tr/ACGT/acgt/;
+    print "\nLowercase: ";
+} elsif ($mode eq "upper") {
+    $dna =~ tr/acgt/ACGT/;
+    print "\nUppercase: ";
+} else {
+    print $usage;
+    exit 1;
+}
+
+print $dna, "\n";
