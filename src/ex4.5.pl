14801b885258951846e99420beb0e4c998175bf1
Alex Smith <alex.smith@redhat.com>
2019-09-02 13:23:17 -0400

    Add solution for Exercise 4.5.

---
+++ src/ex4.5.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl -w
+
+# Exercise 4.5
+#
+# Reverse transcript RNA to DNA
+
+my ($rna, $dna);
+
+$rna = uc(join('', <STDIN>));
+$rna =~ s/\s*//g;
+
+$dna = $rna;
+$dna =~ tr/AUCG/TAGC/;
+$dna = reverse $dna;
+
+print "RNA: ", $rna, "\n";
+print "DNA: ", $dna, "\n";
