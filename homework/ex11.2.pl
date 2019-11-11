adfde0ab12250b6be0656509048c9921162c9062
Alex Smith <alex.smith@redhat.com>
2019-11-10 22:46:49 -0500

    Add solution for exercise 11.2.

---
+++ homework/ex11.2.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+# Exercise 11.2
+#
+# Find all of the Perl files in a filesystem tree using File::Find.
+
+use strict;
+use warnings;
+
+use File::Find;
+use File::Spec;
+
+
+# Find Perl files. This subroutine conforms to the "wanted" callback
+# spec expected by File::Find.
+sub perl_files {
+    if (-f and /\.(pl|pm)$/) {
+        print "Perl file: $File::Find::name\n";
+    }
+}
+
+
+# The MAIN part.
+sub main {
+    my ($dir) = @ARGV;
+    $dir = File::Spec->rel2abs($dir);
+    print "Searching for Perl files in: ${dir}\n";
+    find(\&perl_files, $dir);
+}
+
+
+# Do it!
+main();
